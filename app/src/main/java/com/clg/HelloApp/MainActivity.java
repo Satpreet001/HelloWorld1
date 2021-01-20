9f1ee7d88d88dcd3915dcbe9942e1629d6f11608
Satpreet001 <satpreetsingh2706@gmail.com>
2021-01-20 11:11:52 -0500

    Second commit

--- app/src/main/java/com/clg/HelloApp/MainActivity.java
+++ app/src/main/java/com/clg/HelloApp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    // Testing for commit1111
+    // Testing for commit1
     }
 }
